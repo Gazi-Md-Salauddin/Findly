7d7739cd3a1f9b2e1d171f1f6f908e0e70ae7761
Gazi Md Salauddin <mdsalauddin329132@gmail.com>
2026-09-08 20:27:19 +0600

    Fix client folder submodule issue

---
+++ client/src/app/components/homepage/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <h2>Findly</h2>
+            <div>
+                <p>Home</p>
+                <p>Browse Item</p>
+                <p>Home</p>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ client/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex">
+      <p>Building Findly</p>
+    </div>
+  );
+}
